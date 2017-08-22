716c70f4a2469d916ce1b1e6de9250acd9eeccd1
diabeticus95 <diabeticus95@gmail.com>
2017-08-22 19:15:58 +0200

    dodalem probkowanie

--- main.cpp
+++ main.cpp
@@ -6,13 +6,14 @@
 using namespace std;
 
 int main(int argc, char** argv){
-	double lambda = 632.8*0.000000001;
+	double probkowanie = pow(10,5);
+	double lambda = 632.8*0.000000001 * probkowanie;
 	double k = 2*M_PI/lambda;
-	int bit_depth = 8;
-	int f = 5;
+	int bit_depth = 4;
+	int f = 1 * probkowanie;
 	double coeff = (double)k/(2*f);
-	int MAX_ROW = 512;
-	int MAX_COL = 512;
+	int MAX_ROW = 2048;
+	int MAX_COL = 2048;
 	Bitmap bitmap(MAX_ROW, MAX_COL);
 	int norm = 255;
 	double max_bitmap_value = 0;
@@ -21,7 +22,7 @@ int main(int argc, char** argv){
 	for (int row = 0; row < MAX_ROW; row++){
 		for (int col = 0; col < MAX_COL; col++){
 //		bitmap.bmap[bitmap.index(row,col)] = fmod(- ((-k)/(2*f)) * (pow(row,2)) + pow(col,2), 2*M_PI);
-		bitmap.bmap[bitmap.index(row,col)]	= fmod(coeff*(pow(row,2) + pow(col,2)),10000000000*2*M_PI);
+		bitmap.bmap[bitmap.index(row,col)]	= fmod(coeff*(pow(row,2) + pow(col,2)),2*M_PI);
 //		cout<<"wartosc  "<<bitmap.bmap[bitmap.index(row,col)]<<"  ";
 		if (bitmap.bmap[bitmap.index(row,col)]>max_bitmap_value) max_bitmap_value = bitmap.bmap[bitmap.index(row,col)];
 //		cout<<"max  "<<max_bitmap_value<<endl;
