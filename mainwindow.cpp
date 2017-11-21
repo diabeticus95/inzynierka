90df47863a5a27de01285064309e213360a3088a
diabeticus95 <diabeticus95@gmail.com>
2017-11-21 11:51:05 +0100

    to do: unique -> shared?

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,63 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <memory>
+#include "bmp/EasyBMP.h"
+#include "Bitmap.h"
+#include "Lens.h"
+#include "Zernike.h"
+#include <vector>
+#include <iostream>
+#include <QByteArray>
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
 	ui(new Ui::MainWindow)
 {
 	ui->setupUi(this);
+	for (int i = 0; i < 21; i++){
+			ui->boxZernike->addItem("Z "+QString::number(i));
+	}
+
 }
 
 MainWindow::~MainWindow()
 {
 	delete ui;
+}
+
+void MainWindow::on_button_genLens_clicked()
+{
+	double f = this->ui->textEdit_ogniskowa->toPlainText().toDouble();
+	int size =  this->ui->textEdit_size->toPlainText().toInt();
+	double probkowanie =  (double)1000000/this->ui->textEdit_sampling->toPlainText().toDouble();
+	double lambda =  this->ui->textEdit_lambda->toPlainText().toDouble() * 0.000000001;
+	this->soczewka = std::make_unique<Lens>(size,size, lambda, f, probkowanie, Lens::paraxial_formula);
+	QString nazwaStr = this->ui->lineEdit_nazwa->text();
+	nazwaStr.append(".bmp");
+	QByteArray ba = nazwaStr.toLatin1();
+	char *nazwa = ba.data();
+	soczewka->returnBitmap()->generateImage(nazwa);
+}
+
+void MainWindow::on_push_addToVector_clicked()
+{
+	std::unique_ptr<DiffractiveStructure> zernik = std::make_unique<Zernike>(soczewka->returnBitmap()->row_count,
+																			 soczewka->returnBitmap()->row_count,
+																			 this->ui->lineZernCoeff->text().toDouble(),
+																			 zernFuncs[this->ui->boxZernike->currentIndex()]);
+	toMerge.push_back(zernik.get());
+}
+
+void MainWindow::on_push_generateZern_clicked()
+{
+	toMerge.push_back(soczewka.get());
+	Bitmap merczLens(soczewka->returnBitmap()->row_count,soczewka->returnBitmap()->row_count);
+/*	merczLens.mergeMaps(toMerge);
+	QString nazwaStr = this->ui->lineEdit_nazwaZern->text();
+	nazwaStr.append(".bmp");
+	QByteArray ba = nazwaStr.toLatin1();
+	char *nazwa = ba.data();
+	merczLens.generateImage(nazwa);
+*/
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <memory>
+#include <vector>
+#include "DiffractiveStructure.h"
+#include "Zernike.h"
+#include "Lens.h"
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +20,20 @@ public:
 	explicit MainWindow(QWidget *parent = 0);
 	~MainWindow();
 
+private slots:
+	void on_button_genLens_clicked();
+
+	void on_push_addToVector_clicked();
+
+	void on_push_generateZern_clicked();
+
 private:
 	Ui::MainWindow *ui;
+	std::shared_ptr<DiffractiveStructure> soczewka;
+	std::vector<DiffractiveStructure*> toMerge;
+	typedef double(*zernFunc)(double, double);
+	zernFunc zernFuncs[21] = {Zernike::Z0, Zernike::Z1, Zernike::Z2, Zernike::Z3, Zernike::Z4, Zernike::Z5, Zernike::Z6, Zernike::Z7, Zernike::Z8, Zernike::Z9, Zernike::Z10, Zernike::Z11, Zernike::Z12, Zernike::Z13, Zernike::Z14, Zernike::Z15, Zernike::Z16, Zernike::Z17, Zernike::Z18, Zernike::Z19, Zernike::Z20};
+
 };
 
 #endif // MAINWINDOW_H
